f3f0a30adc0f495d3e0050e554fa8097866a2d24
Pablo Barrales <psbarrales@gmail.com>
2022-02-05 16:23:48 -0300

    fix(entrypoint): double quote to prevent globbing and word splitting

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [ -z $NODE_ENV ]; then
+if [ -z "$NODE_ENV" ]; then
   echo "NODE_ENV is missing"
   exit 1
 fi
 
-if [[ $NODE_ENV = "production" ]]; then
+if [[ "$NODE_ENV" = "production" ]]; then
   yarn prod &
 else
   yarn dev &
